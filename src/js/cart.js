f10b08faa3c4b56bf6e339f607fd4bd524d2332c
macewhit <macymwhitney@gmail.com>
2024-01-17 23:15:28 -0500

    merged main into branch

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,11 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, setClick } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   if (cartItems) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    setClick(".cart-card__remove-btn", removeFromCart)
   } else {
     document.querySelector(".product-list").innerHTML =
       "Your cart is currently empty";
@@ -25,9 +26,19 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <span class="cart-card__remove-btn"> <img data-id="${item.Id}" src="/images/remove-icon.svg"/> </span>
 </li>`;
 
   return newItem;
+}
+
+function removeFromCart (event){
+  const productId = event.target.getAttribute("data-id");
+  const cartItems = getLocalStorage("so-cart");
+  const newItems = cartItems.filter( (item) => {return item.Id !== productId})
+
+  setLocalStorage("so-cart", newItems);
+  renderCartContents();
 }
 
 renderCartContents();
