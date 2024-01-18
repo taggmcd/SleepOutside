6fbd1d7badbd7d24990685b98d127802ed727531
migarcos <migark@hotmail.com>
2024-01-17 21:43:37 -0500

    team2

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getParam } from "./utils.mjs";
+import ProductDetails from './ProductDetails.mjs';
 import ProductData from "./ProductData.mjs";
 
+const productId = getParam('product');
 const dataSource = new ProductData("tents");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
