7096a22b6246fde55b47728f59a1bf662995480e
migarcos <migark@hotmail.com>
2024-01-18 05:15:59 -0500

    adding code to product_pages/index.html and correct product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,9 @@
 import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
-import ProductDetails from './ProductDetails.mjs';
+import ProductDetails from "./ProductDetails.mjs";
 import ProductData from "./ProductData.mjs";
 
 const productId = getParam('product');
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
