cdd0fdc9b07fb3114f50cf9d43371d10726430b3
raeland <rsvarady@gmail.com>
2024-01-20 12:05:24 -0500

    step 5 #2

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -7,6 +7,17 @@ export default class ProductListing {
       this.listElement = listElement;
     }
 
+    function productCardTemplate(product) {
+        return `<li class="product-card">
+        <a href="product_pages/index.html?product=">
+        <img src="" alt="Image of ">
+        <h3 class="card__brand"></h3>
+        <h2 class="card__name"></h2>
+        <p class="product-card__price">$</p>
+        </a>
+        </li>`
+    }
+    
     async init() {
       // our dataSource will return a Promise...so we can use await to resolve it.
       const list = await this.dataSource.getData();
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,7 @@
 import ProductData from "./ProductData.mjs"
 import ProductListing from "./ProductList.mjs"
+
+const listElement = document.querySelector(".product-list")
+let product = new ProductData("tents");
+let listing = new ProductListing("tents, product, listElement");
+await listing.init()
