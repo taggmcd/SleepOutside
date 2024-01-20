7234cb8974f07699c640c0530fa1a7f53059a10b
macewhit <macymwhitney@gmail.com>
2024-01-19 22:36:02 -0500

    remove from cart bug, shows "now empty" text now

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,7 +2,7 @@ import { getLocalStorage, setLocalStorage, setClick } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  if (cartItems) {
+  if (cartItems.length > 0) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
     setClick(".cart-card__remove-btn", removeFromCart);
