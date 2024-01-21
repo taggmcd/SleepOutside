8993048a7d87a766989c1de5a21b7becf8607cf8
migarcos <migark@hotmail.com>
2024-01-21 01:52:08 -0500

    step 3

--- src/js/Alert.mjs
+++ src/js/Alert.mjs
@@ -1,6 +1,18 @@
+// Your class should read the contents of the alerts.js file, 
+// and if it finds anything it should create a 
+// <section class="alert-list"> element then  loop through the results 
+// and build a <p> for each alert and apply the background 
+// and foreground colors to it specified in the alert.
 export default class Alert {
-    constructor() {
+    constructor(content) {
+        this.content = content;
         this.path = `../public/json/alerts.json`;
     }
-    
+    getData() {
+        return fetch(this.path)
+            .then(convertToJson)
+            .then((data) => (data));
+    }
+
+
 }
