f8ec5436446cff75de7ad79bb443ceb14dfc688a
macewhit <macymwhitney@gmail.com>
2024-01-24 23:22:14 -0500

    Duplicate items in cart (quantity)

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -40,7 +40,7 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__quantity">qty: ${item.quantity}</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
   <img class="cart-card__remove-btn" data-id="${item.Id}" src="/images/remove-icon.svg"/>
 </li>`;
