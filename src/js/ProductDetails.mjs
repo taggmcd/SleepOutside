178d33dd0e32f8a3db01f8a3a3c5f2916415dce6
raeland <rsvarady@gmail.com>
2024-01-27 21:12:59 -0500

    all done (i think-i hope)

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -5,7 +5,7 @@ function productDetailsTemplate(product) {
       <h2 class="divider">${product.NameWithoutBrand}</h2>
       <img
         class="divider"
-        src="${product.Image}"
+        src="${product.Image.PrimaryLarge}"
         alt="${product.NameWithoutBrand}"
       />
       <p class="product-card__price">$${product.FinalPrice}</p>
@@ -25,29 +25,8 @@ export default class ProductDetails {
         this.dataSource = dataSource;
     }
 
-    addToCart() {
-        let cart = getLocalStorage("so-cart");
-        if (cart === null) {
-          cart = []; // initialize cart as array
-        }
-
-        const productIds = cart.map(product => product.Id) // list of Ids in the cart
-        const index = productIds.indexOf(this.productId) // finds Id of current product
-
-        if (index == -1) { // check to see if product is not in cart
-          this.product.quantity = 1;
-          cart.push(this.product); // add product to cart
-        }
-        else {
-          cart[index].quantity += 1; // add 1 to quantity of item already in cart
-        }
-
-        setLocalStorage("so-cart", cart);
-    }
-
    async init() {
         this.product = await this.dataSource.findProductById(this.productId);
-
         this.renderProductDetails("main");
         // once the HTML is rendered we can add a listener to Add to Cart button
         // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
@@ -55,6 +34,24 @@ export default class ProductDetails {
         .getElementById("addToCart")
         .addEventListener("click", this.addToCart.bind(this));
     }
+
+    addToCart() {
+      let cart = getLocalStorage("so-cart");
+      if (cart === null) {
+        cart = []; // initialize cart as array
+      }
+      const productIds = cart.map(product => product.Id) // list of Ids in the cart
+      const index = productIds.indexOf(this.productId) // finds Id of current product
+      if (index == -1) { // check to see if product is not in cart
+        this.product.quantity = 1;
+        cart.push(this.product); // add product to cart
+      }
+      else {
+        cart[index].quantity += 1; // add 1 to quantity of item already in cart
+      }
+
+      setLocalStorage("so-cart", cart);
+  }
 
     renderProductDetails(selector) {
         const element = document.querySelector(selector);
