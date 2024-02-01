7406bde66704039b8b39ee184cc8dae62012fee4
fvalto <skualo_fv@msn.com>
2024-02-01 01:00:32 -0300

    we've tried

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,5 +1,33 @@
-import { getLocalStorage } from "./utils.mjs"
-import { displayTotal } from "./ShoppingCart.mjs";
+import { getLocalStorage } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
+
+const services = new ExternalServices();
+
+// takes the items currently stored in the cart (localstorage) and returns them in a simplified form.
+function packageItems(items) {
+// convert the list of products from localStorage to the simpler form required for the checkout process. Array.map would be perfect for this.
+    const simplifiedItems = items.map((item) => {
+        console.log(item);
+        return {
+        id: item.Id,
+        price: item.FinalPrice,
+        name: item.Name,
+        quantity: 1,
+        };
+    });
+    return simplifiedItems;
+}
+
+function formDataToJSON(formElement) {
+    const formData = new FormData(formElement),
+      convertedJSON = {};
+  
+    formData.forEach(function (value, key) {
+      convertedJSON[key] = value;
+    });
+  
+    return convertedJSON;
+  }
 
 export default class CheckoutProcess {
     constructor(key, outputSelector) {
@@ -35,16 +63,45 @@ export default class CheckoutProcess {
       }
     
     displayOrderTotals() {
-    // once the totals are all calculated display them in the order summary page
-    document.getElementById("summary").insertAdjacentElement("afterbegin", renderOrderSummary());
+        // once the totals are all calculated display them in the order summary page
+    // document.getElementById("summary").insertAdjacentElement("afterbegin");
+    const shipping = document.querySelector(this.outputSelector + " #shipping");
+    const tax = document.querySelector(this.outputSelector + " #tax");
+    const orderTotal = document.querySelector(
+      this.outputSelector + " #orderTotal"
+    );
+    shipping.innerText = "$" + this.shipping;
+    tax.innerText = "$" + this.tax;
+    orderTotal.innerText = "$" + this.orderTotal;
     }
+    
+    // renderOrderSummary() {
+    //     const newItem = `<p>Subtotal: $${displayTotal().toFixed(2)}</p>
+    //     <p>Shipping Estimate: $${this.shipping.toFixed(2)}</p>
+    //     <p>Tax: $${this.tax.toFixed(2)}</p>
+    //     <p>Order Total: $${this.orderTotal.toFixed(2)}</p>`;
 
-    renderOrderSummary() {
-        const newItem = `<p>Subtotal: $${displayTotal().toFixed(2)}</p>
-        <p>Shipping Estimate: $${this.shipping.toFixed(2)}</p>
-        <p>Tax: $${this.tax.toFixed(2)}</p>
-        <p>Order Total: $${this.orderTotal.toFixed(2)}</p>`;
-
-        return newItem;
+    //     return newItem;
+    // }
+    
+    async checkout() {
+    // build the data object from the calculated fields, the items in the cart, and the information entered into the form
+    const formElement = document.forms["checkout"];
+ 
+    const json = formDataToJSON(formElement);
+    // add totals, and item details
+    json.orderDate = new Date();
+    json.orderTotal = this.orderTotal;
+    json.tax = this.tax;
+    json.shipping = this.shipping;
+    json.items = packageItems(this.list);
+    console.log(json);
+    try {
+      const res = await services.checkout(json);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+    // call the checkout method in our ExternalServices module and send it our data object.
     }
 }
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -4,11 +4,8 @@ import CheckoutProcess from "./CheckoutProcess.mjs";
 loadHeaderFooter();
 const checkoutProcess = new CheckoutProcess("so-cart", "#summary");
 checkoutProcess.init();
-document
-  .querySelector("#zip")
-  .addEventListener("blur", checkoutProcess.calculateOrdertotal.bind(checkoutProcess));
+document.querySelector("#zip").addEventListener("blur", checkoutProcess.calculateOrdertotal.bind(checkoutProcess));
 document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
     e.preventDefault();
-   
     checkoutProcess.checkout();
 });
