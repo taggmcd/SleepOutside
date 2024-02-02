6c35982555b85fea147871b5380df21c505456d9
Jess Nelson <jemane.19@gmail.com>
2024-02-01 17:35:24 -0700

    W04pt1

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, alertMessage, removeAllAlerts } from "./utils.mjs";
 import ExternalServices from "./ExternalServices.mjs";
 
 const services = new ExternalServices();
@@ -93,8 +93,13 @@ export default class CheckoutProcess {
     try {
       const res = await services.checkout(json);
       console.log(res);
+      setLocalStorage("so-cart", []);
+      location.assign("/checkout/success.html");
     } catch (err) {
-      console.log(err);
+      removeAllAlerts();
+      for (let message in err.message) {
+        alertMessage(err.message[message]);
+      }
     }
     // call the checkout method in our ExternalServices module and send it our data object.
     }
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+let numbVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+const advert = document.createElement('div');
+const ad = document.querySelector('#advert');
+if (numbVisits == 0) {
+        advert.innerHTML = `<h3>Register With Us to Earn More Sleep!</h3>
+        <h4>Giveaway of an item $50 or more for all who register!</h4>`;
+          ad.prepend(advert);
+}
+else {
+    advert.innerHTML = `<h2>Welcome Back!</h2>`;
+    ad.prepend(advert);
+}
+
+numbVisits++;
+
+localStorage.setItem("numVisits-ls", numbVisits);
